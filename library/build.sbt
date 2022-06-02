e11fcf657aefb407f46e17c8d27d2660439859e2
Igor Semergeiev <i.semergeiev@tranzzo.com>
2022-06-02 19:02:21 +0300

    Implemented maven publishing

--- library/build.sbt
+++ library/build.sbt
@@ -1,8 +1,6 @@
 name := "library"
 
-version := "1.1"
-
-scalaVersion := "2.13.8"
+version := "1.2"
 
 libraryDependencies ++= Seq(
   "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
---
+++ project/CommonSettings.scala
@@ -0,0 +1,21 @@
+import sbt.Keys.{externalResolvers, organization, publishTo, scalaVersion}
+import sbt._
+
+object CommonSettings {
+
+  val credentials = Credentials (
+    "GitHub Package Registry",
+    "maven.pkg.github.com",
+    "Semer7",
+    System.getenv("GITHUB_TOKEN")
+  )
+
+  val generalSettings: Seq[Def.Setting[String]] = Seq(
+    organization := "ua.semer7",
+    scalaVersion := "2.13.8"
+  )
+
+  val resolvers = externalResolvers += "GitHub semer7 Maven Repo" at "https://maven.pkg.github.com/semer7/poc"
+
+  val githubMavenPackage = "GitHub semer7 Maven Repo" at "https://maven.pkg.github.com/semer7/poc"
+}
